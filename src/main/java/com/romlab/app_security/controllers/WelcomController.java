6d03817a0d9046b83c10b328a9debf3011bd3e18
HectorARG <hectorr9577@gmail.com>
2025-03-01 14:58:09 -0600

    Initial project with Spring Security
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/romlab/app_security/AppSecurityApplication.java
    #	src/main/resources/application.properties

---
+++ src/main/java/com/romlab/app_security/controllers/WelcomController.java
@@ -0,0 +1,19 @@
+package com.romlab.app_security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/welcome")
+public class WelcomController {
+
+    @GetMapping
+    public Map<String, String> welcome() {
+        return Collections.singletonMap("msj", "Welcome to my application");
+    }
+
+}
