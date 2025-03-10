906efbb8cecc4626d19695c5643e965d1eb7b14b
HectorRoG <hectorr9577@gmail.com>
2025-03-09 21:56:42 -0600

    Inicio de OAUTH2.0

--- src/main/java/com/romlab/app_security/AppSecurityApplication.java
+++ src/main/java/com/romlab/app_security/AppSecurityApplication.java
@@ -2,20 +2,8 @@ package com.romlab.app_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-/* Esta anotación es para incorporar
-* la seguridad ya está colocado por default asi
-* que colocarla ya será dependiendo de
-* cada persona la opcion debug esta esta por
-* defaultnos en false y nos ayuda
-* para saber porque filtros pasann las peticiones
-* NOTA: solo usarlo en desarrollo ya que
-* contiene datos sensibles.
-*/
-//@EnableWebSecurity(debug = true)
-@EnableWebSecurity
 public class AppSecurityApplication {
 
 	public static void main(String[] args) {
