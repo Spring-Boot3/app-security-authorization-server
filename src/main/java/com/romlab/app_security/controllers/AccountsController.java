bed28dfb784f918be2150c075529bcf8c7494b03
HectorRoG <hectorr9577@gmail.com>
2025-03-09 21:56:42 -0600

    Inicio de OAUTH2.0

--- src/main/java/com/romlab/app_security/controllers/AccountsController.java
+++ src/main/java/com/romlab/app_security/controllers/AccountsController.java
@@ -1,7 +1,5 @@
 package com.romlab.app_security.controllers;
 
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
